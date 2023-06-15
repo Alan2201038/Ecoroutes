680f4ca1094afbc014e6c0f2f782b766841b3b93
Tthh123 <gtthh123@gmail.com>
2023-06-15 12:22:41 +0800

    Update dijkstra algo

--- Cars/CAR Router.py
+++ Cars/CAR Router.py
@@ -1,11 +1,13 @@
 import osmnx as ox
 import networkx as nx
 from geopy.geocoders import Nominatim
+from GraphFindingAlgos import Dijkstra
 
 place_name = "Singapore"
 graph = ox.graph_from_place(place_name, network_type="drive")
 print("Number of nodes:", len(graph.nodes))
 print("Number of edges:", len(graph.edges))
+
 
 target_location = (1.4295, 103.835)
 target_locationB = (1.4173, 103.8330)
@@ -13,16 +15,20 @@ target_locationB = (1.4173, 103.8330)
 # Specify the source and target nodes
 node_source = ox.distance.nearest_nodes(graph, target_location[1], target_location[0])
 node_target = ox.distance.nearest_nodes(graph, target_locationB[1], target_locationB[0])
+print(node_target)
+print(node_source)
 
 # Find the shortest path between the source and target nodes
 #TO DO: REPLACE WITH OWN DIJSTRKA ALGORITHM
-shortest_path = nx.shortest_path(graph, node_source, node_target, weight='length')
+shortest_path_correct = nx.shortest_path(graph, node_source, node_target, weight='length')
+shortest_path=Dijkstra.dijkstra(graph,node_source,node_target)
 
 # Print the shortest path
 print("Shortest path:", shortest_path)
+print("Library shortest path:",shortest_path_correct)
 geolocator = Nominatim(user_agent="ecoroutes_test")
 coordinates = []
-for node in shortest_path:
+for node in shortest_path[0]:
     node_data = graph.nodes[node]
     latitude, longitude = node_data['y'], node_data['x']
     location = geolocator.reverse((latitude, longitude), exactly_one=True)
@@ -35,7 +41,7 @@ for node in shortest_path:
 print("Coordinates:", coordinates)
 
 # Plot the graph with the shortest path highlighted
-fig, ax = ox.plot_graph_route(graph, shortest_path, route_linewidth=6, node_size=0, bgcolor='w')
+fig, ax = ox.plot_graph_route(graph, shortest_path[0], route_linewidth=6, node_size=0, bgcolor='w')
 
 # Customize the plot
 ax.scatter(target_location[1], target_location[0], c='r', edgecolor='k', s=100, label='Source')
---
+++ Cars/GraphFindingAlgos/Dijkstra.py
@@ -0,0 +1,50 @@
+
+import math
+
+from Cars.GraphFindingAlgos import minheap
+
+
+def dijkstra (graph,start,end):
+
+  heap = minheap.MinHeap()
+  visited = set()
+  distance_dict={}#Keeps track of the current shortest distance of all vertices from the start node
+  prev_dict={}#Keeps track of the shortest previous node
+  prev_dict[start]=None
+  for node in graph.nodes:
+    if node == start:
+      distance_dict[node] = 0
+    else:
+      distance_dict[node] = float('inf')
+
+
+  heap.insert((start, 0))
+  while not heap.check_empty():
+    current_node, current_distance = heap.get_root()
+
+    if current_node == end:  # dest
+      break
+    if current_node in visited:
+      continue
+
+    visited.add(current_node)
+
+    neighbors = graph.neighbors(current_node)
+    for neighbor in neighbors:
+      edge_data = graph.get_edge_data(current_node, neighbor)
+      edge_weight = edge_data[0]['length']  # Change 'length' to the appropriate weight attribute
+      distance = distance_dict[current_node] + edge_weight
+      if distance < distance_dict[neighbor]:
+        distance_dict[neighbor] = distance
+        prev_dict[neighbor] = current_node
+        heap.insert((neighbor, distance))
+
+  path = []
+  current_node = end
+
+  while current_node:
+    path.append(current_node)
+    current_node = prev_dict[current_node]
+
+  path.reverse()
+  return (path,distance_dict[end])
