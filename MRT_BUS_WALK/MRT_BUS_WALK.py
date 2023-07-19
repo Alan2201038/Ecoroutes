3143af600710df6173e0ce4a6bbb8daf51504650
Tthh123 <gtthh123@gmail.com>
2023-07-19 18:32:34 +0800

    Fix bugs?

--- MRT_BUS_WALK/MRT_BUS_WALK.py
+++ MRT_BUS_WALK/MRT_BUS_WALK.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import networkx as nx
 from geopy.distance import geodesic
+import osmnx as ox
 import matplotlib.pyplot as plt
+import folium
 import pickle
 import os
-from GraphFindingAlgos import AStar_Eco
 mrtGraph= "..\\Data\\MRT_Pickle_Graph"
 buswalkGraph="..\\Data\\Bus_Walk.pickle"
 
@@ -22,5 +23,11 @@ if os.path.exists(mrtGraph) and os.path.exists(buswalkGraph):
         buswalk_G=pickle.load(f)
     combined_G=nx.compose(mrt_G,buswalk_G)
 
-print("Starting")
-
+for node in buswalk_G.nodes():
+    neighbors = buswalk_G.neighbors(node)
+    for neigbor in neighbors:
+        edge_data = buswalk_G.get_edge_data(node, neigbor)[
+            0]  # Get the edge data between current_node and neighbor
+        edge_weight = edge_data.get('duration', float('inf'))
+        edge_transportation = edge_data.get('key', '')[:3]
+        print(edge_data)
